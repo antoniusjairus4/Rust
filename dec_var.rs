bd3adb44f701c599016da4981c0caaea5ac078f1
antoniusjairus4 <antoniusjairusofficial@gmail.com>
2026-05-08 18:39:59 +0530

    Basic operations

---
+++ dec_var.rs
@@ -0,0 +1,15 @@
+fn main()
+{
+    let a = 10;
+    let b = 5;
+
+    let sum = a + b;
+    let product = a * b;
+    let diff = a - b;
+    let div = a / b;
+
+    println!("Sum = {}", sum);
+    println!("Product = {}", product);
+    println!("Difference = {}", diff);
+    println!("Division = {}", div);
+}
