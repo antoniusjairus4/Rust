1ea9f4602fb09885cc4040bc0d620c389cf6f7a6
antoniusjairus4 <antoniusjairusofficial@gmail.com>
2026-05-09 18:53:26 +0530

    Largest of 3 numbers

--- larg.rs
+++ larg.rs
@@ -3,5 +3,17 @@ fn main()
     let a = 5;
     let b = 10;
     let c = 15;
+
+    if a > c && a > c {
+        println!("A is the largest");
+    }
+
+    else if b > a && b > c {
+        println!("B is the greatest");
+    }
+
+    else {
+        println!("C is the greatest");
+    }
     
 }
