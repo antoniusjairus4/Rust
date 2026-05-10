77add177d99e306ff25c90cf23acaf7800ffc27f
antoniusjairus4 <antoniusjairusofficial@gmail.com>
2026-05-10 14:02:02 +0530

    Getting number input

--- Basic/num_inp.rs
+++ Basic/num_inp.rs
@@ -4,7 +4,13 @@ fn main()
 {
     let mut number = String :: new;
 
-    print!("Enter a number: ");
+    print!("Enter a number : ");
 
-    io
+    io :: stdin()
+        .read_line(&mut number)
+        .expect("Failed to read input");
+
+    let number: i32 = number.trim().parse().expect("Invalid number");
+
+    println!("Ther Enrtered number is : {}", number);
 }
