c13f5c63d100825708b9ed1512abd49200f7b2ab
antoniusjairus4 <antoniusjairusofficial@gmail.com>
2026-05-10 14:07:17 +0530

    Fixed an error

--- Basic/num_inp.rs
+++ Basic/num_inp.rs
@@ -1,16 +1,18 @@
-use std :: io;
+use std::io;
+use std::io::Write;
 
 fn main()
 {
-    let mut number = String :: new ();
+    let mut number = String::new();
 
     print!("Enter a number : ");
+    io::stdout().flush().unwrap();
 
-    io :: stdin()
+    io::stdin()
         .read_line(&mut number)
         .expect("Failed to read input");
 
     let number: i32 = number.trim().parse().expect("Invalid number");
 
-    println!("Ther Enrtered number is : {}", number);
+    println!("The Entered number is : {}", number);
 }
